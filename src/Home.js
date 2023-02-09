2b5e10f04c826ecf4357b9bbb90353f869305d7f
Chua Kim Chun <kimchunc@gmail.com>
2023-02-09 22:37:29 +0800

    First commit

---
+++ src/Home.js
@@ -0,0 +1,412 @@
+import React from 'react';
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+  Button,
+  List,
+  Typography,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Box,
+  Grid,
+  Hidden,
+  Switch,
+} from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import Apps from '@material-ui/icons/Apps';
+import MoreVert from '@material-ui/icons/MoreVert';
+import VideoCall from '@material-ui/icons/VideoCall';
+
+import HomeIcon from '@material-ui/icons/Home';
+import Subscriptions from '@material-ui/icons/Subscriptions';
+import Whatshot from '@material-ui/icons/Whatshot';
+
+import VideoLibrary from '@material-ui/icons/VideoLibrary';
+import History from '@material-ui/icons/History';
+
+import AddCircle from '@material-ui/icons/AddCircle';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    backgroundColor: theme.palette.background.dark,
+  },
+  appBar: {
+    boxShadow: 'none',
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  logo: {
+    height: 25,
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+    borderRight: 'none',
+  },
+  menuIcon: {
+    paddingRight: theme.spacing(5),
+    paddingLeft: theme.spacing(6),
+  },
+  icons: {
+    paddingRight: theme.spacing(5),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  listItemText: {
+    fontSize: 14,
+  },
+  listItem: {
+    paddingTop: 4,
+    paddingBottom: 4,
+  },
+  subheader: {
+    textTransform: 'uppercase',
+  },
+}));
+
+const videos = [
+  {
+    id: 1,
+    title:
+      'Teaching Primary School Students',
+    location: 'Pulau NTU',
+    views: '11 mi de visualizações',
+    date: '3 March 2023 - 31 December 2023',
+    time: '7.00pm - 9.00pm',
+    avatar: '/images/avatar.jpeg',
+    thumb: 'logo192.png',
+    vacancy: '20'
+  },
+  {
+    id: 2,
+    title:
+      'Teaching Primary School Students',
+    location: 'Pulau NTU',
+    views: '11 mi de visualizações',
+    date: '3 March 2023 - 31 December 2023',
+    time: '7.00pm - 9.00pm',
+    avatar: '/images/avatar.jpeg',
+    thumb: 'logo192.png',
+    vacancy: '20'
+  }
+];
+
+function Home({ darkMode, setDarkMode }) {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <div className={classes.root}>
+      <AppBar color='inherit' className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuIcon}
+            aria-label='menu'
+          >
+            <MenuIcon />
+          </IconButton>
+          <img
+            src='logo'
+            alt='logo'
+            className={classes.logo}
+          />
+          <div className={classes.grow} />
+          <Switch
+            value={darkMode}
+            onChange={() => setDarkMode(!darkMode)}
+            className={classes.icons}
+          />
+          <IconButton className={classes.icons}>
+            {/* <VideoCall /> */}
+          </IconButton>
+          {/* <IconButton className={classes.icons}>
+            <Apps />
+          </IconButton> */}
+          <IconButton className={classes.icons}>
+            {/* <MoreVert /> */}
+          </IconButton>
+          <Button
+            startIcon={<AccountCircle />}
+            variant='outlined'
+            color='secondary'
+          >
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box display='flex'>
+        <Hidden mdDown>
+          <Drawer
+            className={classes.drawer}
+            variant='permanent'
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+              <List>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>{<HomeIcon />}</ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Home'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>{<Whatshot />}</ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Discover Opportunities'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>{<Subscriptions />}</ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Learning courses'}
+                  />
+                </ListItem>
+              </List>
+              <Divider />
+              <List>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <VideoLibrary />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Favorites'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <History />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'History'}
+                  />
+                </ListItem>
+              </List>
+              <Divider />
+              <Box p={7}>
+                <Typography variant='body2'>
+                Log in to apply for volunteer opportunities
+                </Typography>
+                <Box mt={2}>
+                  <Button
+                    variant='outlined'
+                    color='secondary'
+                    startIcon={<AccountCircle />}
+                  >
+                    Log In
+                  </Button>
+                </Box>
+              </Box>
+              <Divider />
+              {/* <List
+                component='nav'
+                aria-labelledby='nested-list-subheader'
+                subheader={
+                  <ListSubheader
+                    component='div'
+                    id='nested-list-subheader'
+                    className={classes.subheader}
+                  >
+                    O Melhor do youtube
+                  </ListSubheader>
+                }
+              >
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <AddCircle />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Música'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <AddCircle />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Esportes'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <AddCircle />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Jogos'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <AddCircle />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Filmes'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <AddCircle />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Notícias'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <AddCircle />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Ao vivo'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <AddCircle />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Destaques'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>
+                    <AddCircle />
+                  </ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Videos 360'}
+                  />
+                </ListItem>
+              </List> */}
+              {/* <Divider />
+              <ListItem button classes={{ root: classes.listItem }}>
+                <ListItemIcon>
+                  <AddCircle />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{
+                    primary: classes.listItemText,
+                  }}
+                  primary={'Procurar mais'}
+                />
+              </ListItem>
+              <Divider /> */}
+            </div>
+          </Drawer>
+        </Hidden>
+
+        <Box p={8}>
+          <Toolbar />
+          <Typography
+            variant='h5'
+            color='textPrimary'
+            style={{ fontWeight: 600 }}
+          >
+            Recomendados
+          </Typography>
+
+          <Grid container spacing={4}>
+            {videos.map((item, index) => (
+              <Grid item lg={3} md={4} sm={6} xs={12}>
+                <Box>
+                  <img
+                    style={{ width: '100%' }}
+                    alt={item.title}
+                    src={item.thumb}
+                  />
+                  <Box>
+                    <Typography
+                      style={{ fontWeight: 600 }}
+                      gutterBottom
+                      variant='body1'
+                      color='textPrimary'
+                    >
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      display='block'
+                      variant='body2'
+                      color='textSecondary'
+                    >
+                      {item.channel}
+                    </Typography>
+                    <Typography variant='body2' color='textSecondary'>
+                      {`${item.date}`}
+                    </Typography>
+                    <Typography variant='body2' color='textSecondary'>
+                      {`Time:`}
+                    </Typography>
+                    <Typography variant='body2' color='textSecondary'>
+                      {`${item.time}`}
+                    </Typography>
+                    <Typography variant='body2' color='textSecondary'>
+                      {`Skills required:`}
+                    </Typography>
+                    <Typography variant='body2' color='textSecondary'>
+                      {`Vacancy available:  ${item.vacancy}`}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Box>
+    </div>
+  );
+}
+
+export default Home;
